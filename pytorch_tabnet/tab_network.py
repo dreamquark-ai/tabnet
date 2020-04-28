0c4b41759ad08277afd68d4d07e3888d2e9bf1c6
Sebastien Fischman <sebastien.fischman@dreamquark.com>
2020-04-28 15:59:50 +0200

    chore: improve clarity and efficiency of embeddings

--- pytorch_tabnet/tab_network.py
+++ pytorch_tabnet/tab_network.py
@@ -209,28 +209,13 @@ class TabNet(torch.nn.Module):
         self.n_shared = n_shared
         self.virtual_batch_size = virtual_batch_size
 
-        if type(cat_emb_dim) == int:
-            self.cat_emb_dims = [cat_emb_dim]*len(self.cat_idxs)
-        else:
-            # check that all embeddings are provided
-            assert(len(cat_emb_dim) == len(cat_dims))
-            self.cat_emb_dims = cat_emb_dim
-        self.embeddings = torch.nn.ModuleList()
-        for cat_dim, emb_dim in zip(self.cat_dims, self.cat_emb_dims):
-            self.embeddings.append(torch.nn.Embedding(cat_dim, emb_dim))
+        self.embedder = EmbeddingGenerator(input_dim, cat_dims, cat_idxs, cat_emb_dim)
+        self.post_embed_dim = self.embedder.post_embed_dim
 
-        # record continuous indices
-        self.continuous_idx = torch.ones(self.input_dim, dtype=torch.bool)
-        self.continuous_idx[self.cat_idxs] = 0
-
-        if isinstance(cat_emb_dim, int):
-            self.post_embed_dim = self.input_dim + (cat_emb_dim - 1)*len(self.cat_idxs)
-        else:
-            self.post_embed_dim = self.input_dim + np.sum(cat_emb_dim) - len(cat_emb_dim)
-        self.post_embed_dim = np.int(self.post_embed_dim)
         self.tabnet = TabNetNoEmbeddings(self.post_embed_dim, output_dim, n_d, n_a, n_steps,
                                          gamma, n_independent, n_shared, epsilon,
                                          virtual_batch_size, momentum)
+
         self.initial_bn = BatchNorm1d(self.post_embed_dim, momentum=0.01)
 
         # Defining device
@@ -242,22 +227,8 @@ class TabNet(torch.nn.Module):
         self.device = torch.device(device_name)
         self.to(self.device)
 
-    def apply_embeddings(self, x):
-        """Apply embdeddings to raw inputs"""
-        cols = []
-        cat_feat_counter = 0
-        for feat_init_idx, is_continuous in enumerate(self.continuous_idx):
-            # Enumerate through continuous idx boolean mask to apply embeddings
-            if is_continuous:
-                cols.append(x[:, feat_init_idx].view(-1, 1))
-            else:
-                cols.append(self.embeddings[cat_feat_counter](x[:, feat_init_idx].long()))
-                cat_feat_counter += 1
-        post_embeddings = torch.cat(cols, dim=1).float()
-        return post_embeddings
-
     def forward(self, x):
-        x = self.apply_embeddings(x)
+        x = self.embedder(x)
         x = self.initial_bn(x)
         return self.tabnet(x)
 
@@ -405,3 +376,75 @@ class GLU_Layer(torch.nn.Module):
         x = self.bn(x)
         out = torch.mul(x[:, :self.output_dim], torch.sigmoid(x[:, self.output_dim:]))
         return out
+
+
+class EmbeddingGenerator(torch.nn.Module):
+    """
+        Classical embeddings generator
+    """
+
+    def __init__(self, input_dim, cat_dims, cat_idxs, cat_emb_dim):
+        """ This is an embedding module for an entier set of features
+
+        Parameters
+        ----------
+        input_dim : int
+            Number of features coming as input (number of columns)
+        cat_dims : list of int
+            Number of modalities for each categorial features
+            If the list is empty, no embeddings will be done
+        cat_idxs : list of int
+            Positional index for each categorical features in inputs
+        cat_emb_dim : int or list of int
+            Embedding dimension for each categorical features
+            If int, the same embdeding dimension will be used for all categorical features
+        """
+        super(EmbeddingGenerator, self).__init__()
+        if cat_dims == [] or cat_idxs == []:
+            self.skip_embedding = True
+            self.post_embed_dim = input_dim
+            return
+
+        self.skip_embedding = False
+        if isinstance(cat_emb_dim, int):
+            self.cat_emb_dims = [cat_emb_dim]*len(cat_idxs)
+        else:
+            self.cat_emb_dims = cat_emb_dim
+
+        # check that all embeddings are provided
+        if len(self.cat_emb_dims) != len(cat_dims):
+            msg = """ cat_emb_dim and cat_dims must be lists of same length, got {len(self.cat_emb_dims)}
+                      and {len(cat_dims)}"""
+            raise ValueError(msg)
+        self.post_embed_dim = int(input_dim + np.sum(self.cat_emb_dims) - len(self.cat_emb_dims))
+
+        self.embeddings = torch.nn.ModuleList()
+        for cat_dim, emb_dim in zip(cat_dims, self.cat_emb_dims):
+            self.embeddings.append(torch.nn.Embedding(cat_dim, emb_dim))
+
+        # record continuous indices
+        self.continuous_idx = torch.ones(input_dim, dtype=torch.bool)
+        self.continuous_idx[cat_idxs] = 0
+
+    def forward(self, x):
+        """
+        Apply embdeddings to inputs
+        Inputs should be (batch_size, input_dim)
+        Outputs will be of size (batch_size, self.post_embed_dim)
+        """
+        if self.skip_embedding:
+            # no embeddings required
+            return x
+
+        cols = []
+        cat_feat_counter = 0
+        for feat_init_idx, is_continuous in enumerate(self.continuous_idx):
+            # Enumerate through continuous idx boolean mask to apply embeddings
+            if is_continuous:
+                cols.append(x[:, feat_init_idx].float().view(-1, 1))
+            else:
+                cols.append(self.embeddings[cat_feat_counter](x[:, feat_init_idx].long()))
+                cat_feat_counter += 1
+        # concat
+        post_embeddings = torch.cat(cols, dim=1)
+        return post_embeddings
